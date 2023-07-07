0be7b599211b46c030a2b966e45660446a5a0987
Aditya <aditya2002pune@gmail.com>
2023-07-07 12:37:49 +0530

    Program to remove duplicates without hashing

---
+++ Arrays/RemoveDuplicates.cpp
@@ -0,0 +1,16 @@
+class Solution{
+public:
+    int remove_duplicate(int a[],int n){
+        // code here
+        int i=0;
+        for(int j=1;j<n;j++)
+        {
+            if(a[j]!=a[i])
+            {
+                a[i+1]=a[j];
+                i++;
+            }
+        }
+        return i+1;
+    }
+}; // remove duplicates without hashing 
